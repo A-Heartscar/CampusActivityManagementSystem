7d4b0bcbdb17a331f8227bc9c3fd866593c55167
Heartscar <2725516759@qq.com>
2025-11-06 14:13:31 +0800

    添加了Response

--- src/main/java/com/example/CampusActivityManagementsystem/controller/UserController.java
+++ src/main/java/com/example/CampusActivityManagementsystem/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.CampusActivityManagementsystem.controller;
 
 import com.example.CampusActivityManagementsystem.dao.User;
+import com.example.CampusActivityManagementsystem.service.Response;
 import com.example.CampusActivityManagementsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable int id) {
-        return userService.getUserById(id);
+    public Response<User> getUserById(@PathVariable int id) {
+        return Response.newSuccess(userService.getUserById(id));
     }
+
 }
